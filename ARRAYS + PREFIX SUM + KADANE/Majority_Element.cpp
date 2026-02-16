9de6f37376146817cf7051d38862d6c037db5c34
Chinmaye HG <chinmaye115@gmail.com>
2026-02-16 19:49:36 +0530

    Majority element

---
+++ ARRAYS + PREFIX SUM + KADANE/Majority_Element.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int count = 0;
+        int majority = 0;
+
+        for (int &num : nums)
+        {
+            if (count == 0)
+            {
+                majority = num;
+            }
+
+            if (num == majority)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+
+        return majority;
+    }
+};
